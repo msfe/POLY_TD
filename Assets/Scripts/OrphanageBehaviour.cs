cf0170ff9ffe53675a8053cd08b7c39c352983d9
Mattias <Mattias.Folke@gmail.com>
2014-05-24 17:10:28 +0200

    added basic functions for orphanage

---
+++ Assets/Scripts/OrphanageBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrphanageBehaviour : MonoBehaviour {
+
+	private float _health;
+	private bool _isDestroyed;
+
+	// Use this for initialization
+	void Start () {
+		_health = 100;
+		_isDestroyed = false;
+	}
+
+	void repair(float amount){
+			_health += amount;
+			if (_health > 100) {
+					_health = 100;
+			}
+	}
+
+	void damage(float amount){
+		_health -= amount;
+		if (_health < 0) {
+			_isDestroyed = true;
+		}
+	}
+
+	float getHealth(){
+		return _health;
+	}
+
+	bool isDestoryed(){
+		return _isDestroyed;
+	}
+}
