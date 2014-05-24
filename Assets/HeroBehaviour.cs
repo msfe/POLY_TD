a4fd126fa49798dfc400a343267608d54a5214f6
Daniel <daniel.maansson@gmail.com>
2014-05-24 16:11:08 +0200

    Basic hero movement

--- Assets/HeroBehaviour.cs
+++ Assets/HeroBehaviour.cs
@@ -3,13 +3,52 @@ using System.Collections;
 
 public class HeroBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public float MaxMoveForce;
+    public float MoveForce;
+    public float Torque;
+
+    private Vector2 _moveTarget;
+    private bool selected;
+    private Vector2 _targetLookAt;
+
+	void Start () 
+    {
+	    
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        if (Input.GetMouseButton(0))
+        {
+             Move(Vec.xy(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+        }
+        else if (Input.GetMouseButton(1))
+        {
+            Vector2 mousePos = Vec.xy(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            Vector2 toMouse = mousePos - Vec.xy(transform.position);
+            toMouse.Normalize();
+            _targetLookAt = toMouse;
+        }
 	}
+
+    void FixedUpdate()
+    {
+        Vector2 toTarget = _moveTarget - Vec.xy(transform.position);
+        float dist = toTarget.magnitude;
+        toTarget /= dist;
+        dist = Mathf.Clamp(dist * MoveForce, 0, MaxMoveForce);     
+        rigidbody2D.AddForce(dist * toTarget);
+
+        Vector2 look = transform.rotation * Vector3.up;
+        Vector2 lookPerp = Vec.PerpendicularCW(look);
+        float rot = Vector2.Dot(lookPerp, _targetLookAt);
+        rigidbody2D.AddTorque(rot * Torque);
+    }
+
+    void Move(Vector2 target)
+    {
+        _moveTarget = target;
+        _targetLookAt = target - Vec.xy(transform.position);
+        _targetLookAt.Normalize();
+    }
 }
