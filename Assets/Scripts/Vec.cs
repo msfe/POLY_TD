f28794fba88b1b47e593bb1fffe6a7defc5e2a00
Daniel <daniel.maansson@gmail.com>
2014-05-24 14:49:59 +0200

    Vector helper

---
+++ Assets/Scripts/Vec.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+
+public class Vec
+{
+    public static Vector2 xy(Vector3 v)  
+    {
+        return new Vector2(v.x, v.y);
+    }
+    public static Vector2 yz(Vector3 v)
+    {
+        return new Vector2(v.y, v.z);
+    }
+    public static Vector2 xz(Vector3 v)
+    {
+        return new Vector2(v.x, v.z);
+    }
+    public static Vector2 yx(Vector3 v)
+    {
+        return new Vector2(v.y, v.x);
+    }
+    public static Vector2 zy(Vector3 v)
+    {
+        return new Vector2(v.z, v.y);
+    }
+    public static Vector2 zx(Vector3 v)
+    {
+        return new Vector2(v.z, v.x);
+    }
+    public static Vector2 PerpendicularCCW(Vector2 v)
+    {
+        return new Vector2(-v.y, v.x);
+    }
+    public static Vector2 PerpendicularCW(Vector2 v)
+    {
+        return new Vector2(v.y, -v.x);
+    }
+}
+
