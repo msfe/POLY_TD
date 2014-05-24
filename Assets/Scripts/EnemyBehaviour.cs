9a970f964d264cef5f891ff88cdcef0215bc67c0
Daniel <daniel.maansson@gmail.com>
2014-05-24 14:37:14 +0200

    Initial commit

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        rigidbody2D.AddForce(new Vector2(3, 0));
+    }
+}
---
+++ Assets/Scripts/TestBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestBehaviour : MonoBehaviour {
+
+    public Transform other;
+    public float korv;
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void FixedUpdate()
+    { 
+    
+    }
+
+
+}
