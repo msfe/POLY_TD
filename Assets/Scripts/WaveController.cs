d8fa6e7eeee9b1ace79410bc88d282dae2f5f802
Mattias <Mattias.Folke@gmail.com>
2014-05-25 15:04:00 +0200

    added dynamic spawning increase and restart of level when all enemies spawned

--- Assets/Scripts/WaveController.cs
+++ Assets/Scripts/WaveController.cs
@@ -6,10 +6,13 @@ public class WaveController : MonoBehaviour {
     public float Interval = 7f;
     public Waypoints _waypoints;
 
+	private int ENEMIES_TO_SPAWN = 200;
+
     public Transform _singleEnemyPrefab;
     public Transform _simpleGroupPrefab;
 
     float _accumulator;
+	int enemyCounter;
     Vector2 _spawn;
 
 	void Start () 
@@ -19,18 +22,25 @@ public class WaveController : MonoBehaviour {
 	}
 	
 	void Update() 
-    {
+    {	
+		if (allEnemiesSpawned ()) {
+			Application.LoadLevel(0); //TODO handle winning correct
+		}
         _accumulator += Time.deltaTime;
         if (_accumulator > Interval)
         {
             _accumulator -= Interval;
 
             SpawnSingle();
+			if(Interval > 0.15){
+				Interval = Interval * 19f/20f;
+			}
         }
 	}
 
     void SpawnSingle()
-    {
+    {	
+		enemyCounter++;
 		Transform enemyT = (Transform)Instantiate(_singleEnemyPrefab, _spawn - new Vector2(0,-0.3f), Quaternion.identity);
         Transform groupT = (Transform)Instantiate(_simpleGroupPrefab, _spawn, Quaternion.identity);
 
@@ -41,4 +51,8 @@ public class WaveController : MonoBehaviour {
 		group.GetComponent<GroupMembers> ().enemies = new System.Collections.Generic.List<EnemyBehaviour> ();
 		group.GetComponent<GroupMembers>().enemies.Add(enemy);
     }
+
+	public bool allEnemiesSpawned(){
+		return enemyCounter >= ENEMIES_TO_SPAWN;
+	}
 }
