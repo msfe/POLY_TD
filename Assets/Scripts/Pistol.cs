7cb39dc7db1a56b0f0a06b49e1a44182bb710ab6
Mattias <Mattias.Folke@gmail.com>
2014-05-25 14:48:42 +0200

    added pistoler

---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pistol : MonoBehaviour {
+	
+	public Transform BulletPrefab;
+	private HeroBehaviour hero;
+	
+	private bool ATTACK_WHILE_MOVING = true;
+	private float ATTACK_DOWNTIME = 50.0f;
+	private float ATTACK_MOUNTTIME = 00.0f; 
+	private float BULLET_SPREAD = 0.1f;
+	private float BULLET_SPEED = 5.0f;
+	private float BULLET_DAMAGE = 6.0f;
+	private int BULLETS_PER_ATTACK = 1;
+	
+	private float fireCounter;
+	private float mountCounter;
+	
+	// Use this for initialization
+	void Start () {
+		hero = this.GetComponent<HeroBehaviour> ();
+		fireCounter = 0;
+		mountCounter = 0;
+	}
+	
+	void Update () {
+		if (hero.isStanding() || ATTACK_WHILE_MOVING) {
+			if (fireCounter <= 0 && mountCounter <= 0) {
+				for(int i = 0; i<BULLETS_PER_ATTACK;i++){
+					Attack ();
+				}
+				fireCounter = ATTACK_DOWNTIME;
+			} else {
+				mountCounter--;
+			}
+		} else {
+			mountCounter = ATTACK_MOUNTTIME;
+		}
+		if (fireCounter > 0) 
+		{
+			fireCounter--;
+		}
+	}
+	
+	public void Attack(){
+		Transform t = (Transform)Instantiate (BulletPrefab, transform.position, Quaternion.identity);
+		BulletData bullet = t.GetComponent<BulletData> ();
+		bullet.Damage = BULLET_DAMAGE;
+		bullet.AutoAiming = false;
+		Vector2 dir = Vec.xy (transform.rotation * Vector3.up) + BULLET_SPREAD * Random.insideUnitCircle;
+		dir.Normalize ();
+		t.rigidbody2D.velocity = -BULLET_SPEED * dir;
+	}
+	
+}
